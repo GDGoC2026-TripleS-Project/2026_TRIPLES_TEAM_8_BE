ba3c2f82411de63f1a7e25f5e545d2bd359d39d5
H-WooChan <hwc0314@gmail.com>
2026-01-21 03:24:28 +0900

    feat/#01: swagger api 구축

--- src/main/java/com/example/gread/app/TestController.java
+++ src/main/java/com/example/gread/app/TestController.java
@@ -4,20 +4,22 @@ import com.example.gread.global.code.ErrorCode;
 import com.example.gread.global.code.SuccessCode;
 import com.example.gread.global.exception.BusinessException;
 import com.example.gread.global.responseTemplate.ApiResponseTemplate;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "test", description = "테스트")
 @RestController
 @RequestMapping("/test") //공통응답포멧 테스트 코드입니다 필요없을시 삭제하고 시작하세요
 public class TestController {
-
+    @Tag(name = "test", description = "테스트")
     @GetMapping("/ok")
     public ResponseEntity<ApiResponseTemplate<String>> ok() {
         return ApiResponseTemplate.success(SuccessCode.OK, "hello");
     }
-
+    @Tag(name = "test", description = "테스트")
     @GetMapping("/biz-error")
     public ResponseEntity<ApiResponseTemplate<Object>> bizError() {
         throw new BusinessException(ErrorCode.INTERNAL_SERVER_EXCEPTION);
---
+++ src/main/java/com/example/gread/global/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.gread.global.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//http://localhost:8080/swagger-ui/index.html
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "G.reed API", version = "v1.0.0"),
+        security = @SecurityRequirement(name = "BearerAuth")
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi authApi() {
+        return GroupedOpenApi.builder()
+                .group("Auth API")
+                .pathsToMatch("/test/**")
+                .build();
+    }
+
+}
