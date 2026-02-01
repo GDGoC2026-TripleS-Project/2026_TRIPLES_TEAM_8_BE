de0602e6f3cd83ea3727b9597a0d51463ee8b156
parkyumeee <pym@office.skhu.ac.kr>
2026-02-02 00:55:44 +0900

    feat: 리뷰구현 Issues #4

--- src/main/java/com/example/gread/app/review/repository/UserRepository.java
+++ src/main/java/com/example/gread/app/review/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.example.gread.app.review.repository;
 
-import com.example.gread.app.review.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
