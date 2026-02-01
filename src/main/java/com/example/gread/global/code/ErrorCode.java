6576b92718ce0d3c106758cde1f583599362e92e
H-WooChan <hwc0314@gmail.com>
2026-02-01 23:42:20 +0900

    Feat/#1: 마이페이지 구현

--- src/main/java/com/example/gread/global/code/ErrorCode.java
+++ src/main/java/com/example/gread/global/code/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     VALIDATION_EXCEPTION(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-    INTERNAL_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류");
+    INTERNAL_SERVER_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "서버 오류"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저가 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
