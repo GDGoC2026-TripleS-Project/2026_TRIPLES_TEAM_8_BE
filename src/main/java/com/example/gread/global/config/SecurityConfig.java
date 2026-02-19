b438fc1df051b0d9d2ba62106d51c73ae3269c0f
leebohyeon <lilddy35@gmail.com>
2026-02-19 11:58:08 +0900

    refactor book

--- src/main/java/com/example/gread/global/config/SecurityConfig.java
+++ src/main/java/com/example/gread/global/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/favicon.ico").permitAll()
                         .requestMatchers("/api/login/**", "/oauth2/**", "/login/oauth2/**").permitAll()
                         .requestMatchers("/api/feed/explore").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/books/**").permitAll()
                         .anyRequest().authenticated()
                 )
 
